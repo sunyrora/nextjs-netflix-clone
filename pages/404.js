fdb40bd8d8ba23b8ca2dd531321565f3ad7338a8
sunyrora <sunyrora@gmail.com>
2022-07-17 14:15:05 +0200

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+
+const PageNotFound = () => {
+  const [count, setCount] = useState(3);
+  const router = useRouter();
+
+  useEffect(() => {
+    const countdouwn = () => {
+      if (count > 0) {
+        setCount((prev) => prev - 1);
+      }
+    };
+
+    const interval = setInterval(countdouwn, 1000);
+    if (count <= 0) {
+      clearInterval(interval);
+      router.push('/');
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
+
+  return (
+    <div className="text-center">
+      <div className="text-3xl">404 - Page Not Found</div>
+      <div className="text-2xl">Redirect to Home...{count}</div>
+    </div>
+  );
+};
+
+export default PageNotFound;
